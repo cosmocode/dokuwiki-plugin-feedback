9603a49ee52eedd45802e3b8a430dd8a0c1ab7f1
Andreas Gohr <gohr@cosmocode.de>
2014-02-27 10:26:52 +0100

    added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * English language file for feedback plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+$lang['menu']         = 'Feedback Managers festlegen';
+
+$lang['feedback']     = 'Feedback geben';
+$lang['subject']      = 'Feedback';
+$lang['thanks']       = 'Vielen Dank für Ihr Feedback.';
+
+$lang['namespace']    = 'Namensraum';
+$lang['email']        = 'E-Mail';
+$lang['save']         = 'Speichern';
+$lang['saved']        = 'Ihre Feedback Manager wurden gespeichert';
+
+$lang['js']['title']  = 'Bitte geben Sie uns Ihr Feedback';
+$lang['js']['cancel'] = 'Abbrechen';
+$lang['js']['close']  = 'Schließen';
+$lang['js']['submit'] = 'Senden';
+
---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * english language file for feedback plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+$lang['allowanon'] = 'Allow anonymous users to send feedback';

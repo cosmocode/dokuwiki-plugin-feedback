cf8582cc21e7c66ae6e29e482e33bf419894f46d
Andreas Gohr <gohr@cosmocode.de>
2014-03-25 16:07:13 +0100

    adjusted some language strings

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -9,14 +9,14 @@ $lang['menu']         = 'Feedback Managers festlegen';
 
 $lang['feedback']     = 'Feedback geben';
 $lang['subject']      = 'Feedback';
-$lang['thanks']       = 'Vielen Dank für Ihr Feedback.';
+$lang['thanks']       = 'Vielen Dank für Ihre Unterstützung!';
 
 $lang['namespace']    = 'Namensraum';
 $lang['email']        = 'E-Mail';
 $lang['save']         = 'Speichern';
 $lang['saved']        = 'Ihre Feedback Manager wurden gespeichert';
 
-$lang['js']['title']  = 'Bitte geben Sie uns Ihr Feedback';
+$lang['js']['title']  = 'Schreiben Sie uns Ihr Feedback';
 $lang['js']['cancel'] = 'Abbrechen';
 $lang['js']['close']  = 'Schließen';
 $lang['js']['submit'] = 'Senden';

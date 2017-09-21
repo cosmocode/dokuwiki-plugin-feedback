c16050fe92242d0cfc4008d2c94318a956b86f47
Szymon Olewniczak <solewniczak@rid.pl>
2017-09-21 11:13:49 +0200

    adds confing option

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,3 +8,4 @@
 $lang['allowanon'] = 'Allow anonymous users to send feedback';
 $lang['include_parent_startpage'] = 'Include the startpages of namespaces even if they are outside of the namespace';
 $lang['span_translations'] = 'Feedback-Managers always handle all translated versions of their namespace';
+$lang['send_copy'] = 'Send a copy of the feedback to the author (works only for login users)';

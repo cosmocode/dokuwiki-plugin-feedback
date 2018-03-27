5bc7ebda0e9733eca6164b38d96a2fa74e1b25fb
Wojciech Lichota <wojciech@lichota.pl>
2018-03-27 12:39:51 +0200

    Polish translation

---
+++ lang/pl/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Polish language file for feedback plugin
+ *
+ * @author Wojciech Lichota <wojciech@lichota.pl>
+ */
+$lang['menu']         = 'Ustaw menedżerów opinia';
+$lang['feedback']     = 'Wyślij opinie';
+$lang['subject']      = 'Opinia';
+$lang['thanks']       = 'Dziękujemy za podzielenie się opinią.';
+$lang['error']        = 'Niestety wystąpił błąd i Twoja opinia nie mogła zostać wysłana. 😞';
+$lang['namespace']    = 'Katalog';
+$lang['email']        = 'e-mail';
+$lang['save']         = 'Zapisz';
+$lang['saved']        = 'Twoja opinia została zapisana';
+$lang['js']['title']  = 'Podziel się opinią';
+$lang['js']['cancel'] = 'Anuluj';
+$lang['js']['close']  = 'Zamknij';
+$lang['js']['submit'] = 'Wyślij';

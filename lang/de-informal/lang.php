8d3c422869882118d31089249b87242a1f923ffd
Volker <volker@vrbdev.eu>
2026-01-26 21:34:36 +0100

    Add language files for de-informal

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * de-informal language file for feedback plugin
+ *
+ * @author Volker Bürckel <volker@vrbdev.eu>
+ */
+
+$lang['menu']         = 'Feedback-Verantwortliche festlegen';
+
+$lang['feedback']     = 'Feedback geben';
+$lang['subject']      = 'Feedback';
+$lang['thanks']       = 'Vielen Dank für deine Unterstützung!';
+$lang['error']        = 'Leider ist ein Fehler aufgetreten und dein Feedback konnte nicht gesendet werden.';
+
+$lang['namespace']    = 'Namensraum';
+$lang['email']        = 'E-Mail';
+$lang['save']         = 'Speichern';
+$lang['saved']        = 'Deine Feedback-Verantwortlichen wurden gespeichert';
+
+$lang['js']['title']  = 'Schreibe hier dein Feedback';
+$lang['js']['cancel'] = 'Abbrechen';
+$lang['js']['close']  = 'Schließen';
+$lang['js']['submit'] = 'Senden';
